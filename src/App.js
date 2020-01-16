4745af94e527194ef041a0785e07562961a666bf
kevinyohe <kevinyohe7@gmail.com>
2020-01-15 21:21:14 -0500

    Added new context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useContext, useReducer} from "react";
+import { UserContext } from "./index";
+
+const initialState = {
+  count: 0
+}
+
+function reducer(state, action){
+  switch(action.type){
+    case "increment":
+      return{
+        count: state.count +1
+      }
+      case "decrement":
+        return{
+          count: state.count-1
+        }
+        case "reset":
+          return{
+            count: 0
+          }
+    default:
+      return initialState
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const value  = useContext(UserContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      Count: {state.count}
+       <button onClick={()=> dispatch({type: "increment"})}>Increment</button>
+       <button onClick={()=> dispatch({type: "decrement"})}>Decrement</button>
+       <button onClick={()=> dispatch({type: "reset"})}>reset</button>
     </div>
   );
 }
